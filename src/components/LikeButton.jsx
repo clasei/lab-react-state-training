0514c8dff7c65c55702c2ea2016876cd31d7129f
clara seijo <clasei@proton.me>
2024-09-18 15:16:39 +0200

    iteration 1 done

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+
+function LikeButton() {
+
+  const [likes, setLikes] = useState(0) // starts with 0 Likes
+
+  const handleLike = () => {
+    setLikes(likes + 1)
+  } 
+
+  return (
+
+    <button onClick={handleLike}>{likes} Likes</button>
+
+  )
+}
+
+export default LikeButton
